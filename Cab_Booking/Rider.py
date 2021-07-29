e8f16a7e67052f15c1fa931c8e76746ef2f33d9a
pandey-ankur-au17 <ankurpandey131@gmail.com>
2021-07-29 11:16:54 +0530

    Rider module updated

--- Cab_Booking/Rider.py
+++ Cab_Booking/Rider.py
@@ -3,6 +3,12 @@ class Rider:
         self.name=input("Enter Rider Name:")
         self.age=int(input("Enter the Age:"))
         self.mobile_no=int(input("Enter the Mobile Number:"))
+        self.rider_coord=input("Enter your location in x y with space:").split()
+        self.tripDetail=None
+        self.trip_Id = None
+        self.search_Status = None
+        self.rider_Status = True
+
     def rider_details(self):
-        return self.name,self.age,self.mobile_no
+        return self.name,self.age,self.mobile_no,self.rider_coord,self.tripDetail,self.trip_Id,self.search_Status,self.rider_Status
 
